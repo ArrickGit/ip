3fe206b57527ab74df189a5342c403cd2cebc0b7
ArrickGit <arrickang2001@gmail.com>
2026-01-20 00:18:49 +0800

    Completed level 2. Add, List

--- src/main/java/Pompompurin.java
+++ src/main/java/Pompompurin.java
@@ -2,11 +2,8 @@ import java.util.*;
 public class Pompompurin {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-//        String logo = " ____        _        \n"
-//                + "|  _ \\ _   _| | _____ \n"
-//                + "| | | | | | | |/ / _ \\\n"
-//                + "| |_| | |_| |   <  __/\n"
-//                + "|____/ \\__,_|_|\\_\\___|\n";
+        String[] database = new String[100];
+        int databasePointer = 0;
         // ascii art obtain from https://patorjk.com/software/taag/#p=display&f=Big&t=Pompompurin&x=none&v=4&h=4&w=80&we=false
         String logo = "_____                                                       _       \n"
         + "|  __ \\                                                     (_) \n"
@@ -31,8 +28,18 @@ public class Pompompurin {
                 System.out.println(line);
                 break;
             }
+            if (input.equals("list")) {
+                System.out.println(line);
+                for (int i = 0; i < databasePointer; i++) {
+                    System.out.println((i + 1) + ". " + database[i]);
+                }
+                System.out.println(line);
+                continue;
+            }
             System.out.println(line);
-            System.out.println(input);
+            database[databasePointer] = input;
+            databasePointer++;
+            System.out.println("added: " + input);
             System.out.println(line);
         }
     }
